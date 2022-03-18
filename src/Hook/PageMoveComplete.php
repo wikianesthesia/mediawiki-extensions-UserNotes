052eef46668cfb45a503a3c4a0d9b275feb86432
Chris Rishel <rishel@gmail.com>
2022-03-17 22:55:13 -0700

    Improved MovePageComplete hook handling.

--- src/Hook/PageMoveComplete.php
+++ src/Hook/PageMoveComplete.php
@@ -2,90 +2,96 @@
 
 namespace UserNotes\Hook;
 
-use ApiMain;
-use FauxRequest;
 use MediaWiki\Linker\LinkTarget;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\User\UserIdentity;
+use PracticeGroups\PracticeGroups;
+use Status;
 use Title;
-use UserNotes\UserNotes;
 
 class PageMoveComplete {
     public static function callback( LinkTarget $old, LinkTarget $new, UserIdentity $userIdentity, int $pageid, int $redirid, string $reason, RevisionRecord $revision  ) {
-        $movePageFactory = MediaWikiServices::getInstance()->getMovePageFactory();
-        $logger = LoggerFactory::getInstance( UserNotes::getExtensionName() );
-
-        // Prevent infinite recursion
-        if( $old->getNamespace() == NS_USERNOTES ) {
+        if( $old->getNamespace() === NS_USERNOTES ) {
+            # Prevent infinite recursion
             return;
         }
 
-        // TODO can this be done without FauxRequesting the API?
-        // Currently this fetches all pages within the UserNotes namespace which could be expensive.
-        $resultData = [
-            'continue' => [
-                'apcontinue' => ''
-            ]
+        $dbr = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnectionRef( DB_REPLICA );
+
+        $table = 'page';
+        $vars = [
+            'page_title'
         ];
 
-        while( isset( $resultData[ 'continue' ] ) ) {
-            $request = [
-                'action' => 'query',
-                'list' => 'allpages',
-                'apnamespace' => NS_USERNOTES,
-                'apcontinue' => $resultData[ 'continue' ][ 'apcontinue' ],
-                'aplimit' => 'max'
-            ];
+        // These two like conditions will search for titles which are subpages with exactly one level of depth
+        $conds = [
+            'page_namespace' => NS_USERNOTES,
+            'page_title' . $dbr->buildLike( $dbr->anyString(), '/' . $old->getDBkey() ),
+            'page_title NOT' . $dbr->buildLike( $dbr->anyString(), '/', $dbr->anyString(), '/', $dbr->anyString() )
+        ];
 
-            $fauxReq = new FauxRequest( $request );
+        $res = $dbr->select(
+            $table,
+            $vars,
+            $conds,
+            __METHOD__
+        );
 
-            $module = new ApiMain( $fauxReq );
-            $module->execute();
+        foreach( $res as $row ) {
+            $oldUserNotesTitle = Title::newFromText( $row->page_title, NS_USERNOTES );
 
-            $resultData = $module->getResult()->getResultData();
-            $pageData = $module->getResult()->getResultData( [ 'query', 'allpages' ] );
+            // Get any subpages for the existing title.
+            // Getting now since not sure if getSubpages() will still work after the move.
+            $oldUserNotesTitleSubpages = $oldUserNotesTitle->getSubpages();
 
-            // Iterate through all pages in the UserNotes namespace to find subpages which match the moved article title
-            foreach( $pageData as $pageResult ) {
-                if( isset( $pageResult[ 'pageid' ] ) ) {
-                    $userNotesTitle = Title::newFromID( $pageResult[ 'pageid' ] );
+            // Create a new title for the subpage which matches the new article title
+            $newUserNotesTitle = Title::makeTitleSafe( NS_USERNOTES, $oldUserNotesTitle->getRootText() . '/' . $new->getText() );
 
-                    // Only subpages are relevant since they might match the moved article title
-                    if( $userNotesTitle->isSubpage() ) {
-                        // See if the subpage text matches the old article title
-                        if( $old->getText() == UserNotes::getMainArticleTitle( $userNotesTitle ) ) {
-                            // Create a new title for the subpage which matches the new article title
-                            $newUserNotesTitle = Title::makeTitleSafe( NS_USERNOTES, $userNotesTitle->getRootText() . '/' . $new->getText() );
+            $moveResult = static::movePage( $oldUserNotesTitle, $newUserNotesTitle, $userIdentity );
 
-                            $movePage = $movePageFactory->newMovePage( $userNotesTitle, $newUserNotesTitle );
+            if( $moveResult && count( $oldUserNotesTitleSubpages ) ) {
+                foreach( $oldUserNotesTitleSubpages as $oldUserNotesTitleSubpage ) {
+                    $newUserNotesTitleSubpageText = str_replace(
+                        $oldUserNotesTitleSubpage->getText(),
+                        $newUserNotesTitle->getText(),
+                        $oldUserNotesTitleSubpage->getText()
+                    );
 
-                            $moveValidResult = $movePage->isValidMove();
+                    $newUserNotesTitleSubpage = Title::makeTitleSafe( NS_USERNOTES, $newUserNotesTitleSubpageText );
 
-                            if( !$newUserNotesTitle->exists() && $moveValidResult->isOK() ) {
-                                // Move the subpage
-                                $movePage->move( $userIdentity, wfMessage( 'usernotes-move-reason-mainarticlemoved' )->text(), false );
-                            } else {
-                                # TODO i18n
-                                $errorDetails = '';
-
-                                if( $newUserNotesTitle->exists() ) {
-                                    $errorDetails = 'New page already exists';
-                                } elseif( !$moveValidResult->isOK() ) {
-                                    $errorDetails = $moveValidResult->getMessage()->text();
-                                }
-
-                                $logger->warning( 'Could not move subpage from {old} to {new}: {details}', [
-                                    'old' => $old->getText(),
-                                    'new' => $new->getText(),
-                                    $errorDetails
-                                ] );
-                            }
-                        }
-                    }
+                    // Don't bother checking the result since we should keep trying the other subpages even if one fails.
+                    static::movePage( $oldUserNotesTitleSubpage, $newUserNotesTitleSubpage, $userIdentity );
                 }
             }
+        }
+    }
+
+    protected static function movePage( Title $oldTitle, Title $newTitle, UserIdentity $userIdentity ): Status {
+        $movePageFactory = MediaWikiServices::getInstance()->getMovePageFactory();
+        $logger = LoggerFactory::getInstance( PracticeGroups::getExtensionName() );
+
+        $movePage = $movePageFactory->newMovePage( $oldTitle, $newTitle );
+        $moveValidResult = $movePage->isValidMove();
+
+        $loggerContext = [
+            'old' => $oldTitle->getFullText(),
+            'new' => $newTitle->getFullText(),
+        ];
+
+        // Make sure the move is valid (will check to make sure new title doesn't already exist)
+        if( $moveValidResult->isOK() ) {
+            $logger->debug( 'Attempting to move page from {old} to {new} to preserve main title link', $loggerContext );
+
+            // Move the subpage. Redirects make things complicated (e.g. can't move and then move back)
+            return $movePage->move( $userIdentity, wfMessage( 'practicegroups-move-reason-maintitlemoved' )->text(), false );
+        } else {
+            $loggerContext[ 'details' ] = $moveValidResult->getMessage()->text();
+
+            $logger->error( 'Could not move subpage from {old} to {new}: {details}', $loggerContext );
+
+            return $moveValidResult;
         }
     }
 }
